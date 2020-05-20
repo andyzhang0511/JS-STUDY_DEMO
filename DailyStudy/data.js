54eaae4cd21f180937c0b3bf2791e634e1eb9e91
张帆 <476472974@qq.com>
2020-05-20 18:28:33 +0800

    first commit

---
+++ DailyStudy/data.js
@@ -0,0 +1,36 @@
+const data = [
+  [
+    { matchTime: '2019-01-01', alarmCount: 2 },
+    { matchTime: '2019-01-02', alarmCount: 2 },
+    { matchTime: '2019-01-03', alarmCount: 2 },
+    { matchTime: '2019-01-04', alarmCount: 2 },
+    { matchTime: '2019-01-05', alarmCount: 2 },
+    { matchTime: '2019-01-06', alarmCount: 2 },
+    { matchTime: '2019-01-07', alarmCount: 2 },
+    { matchTime: '2019-01-08', alarmCount: 2 },
+    { matchTime: '2019-01-09', alarmCount: 2 },
+    { matchTime: '2019-01-10', alarmCount: 2 },
+    { matchTime: '2019-01-11', alarmCount: 2 },
+    { matchTime: '2019-01-12', alarmCount: 2 },
+    { matchTime: '2019-01-13', alarmCount: 2 },
+    { matchTime: '2019-01-14', alarmCount: 2 },
+    { matchTime: '2019-01-15', alarmCount: 2 },
+    { matchTime: '2019-01-16', alarmCount: 2 },
+    { matchTime: '2019-01-17', alarmCount: 2 },
+    { matchTime: '2019-01-18', alarmCount: 2 },
+    { matchTime: '2019-01-19', alarmCount: 2 },
+    { matchTime: '2019-01-20', alarmCount: 2 },
+    { matchTime: '2019-01-21', alarmCount: 2 },
+    { matchTime: '2019-01-22', alarmCount: 2 },
+    { matchTime: '2019-01-23', alarmCount: 2 },
+    { matchTime: '2019-01-24', alarmCount: 2 },
+    { matchTime: '2019-01-25', alarmCount: 2 },
+    { matchTime: '2019-01-26', alarmCount: 2 },
+    { matchTime: '2019-01-27', alarmCount: 2 },
+    { matchTime: '2019-01-28', alarmCount: 2 },
+    { matchTime: '2019-01-29', alarmCount: 2 },
+    { matchTime: '2019-01-30', alarmCount: 2 },
+    { matchTime: '2019-01-31', alarmCount: 2 }
+  ]
+]
+export default data
